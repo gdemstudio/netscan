fcffa0be0ee0196b81ec39373f1a80090f12ec7e
BlogByteSecurity <44734920+blogbytesecurity@users.noreply.github.com>
2021-03-09 14:34:24 +0100

    /src/

---
+++ utils.py
@@ -0,0 +1,31 @@
+import json
+from multiprocessing.pool import ThreadPool
+import os
+
+from rich.console import Console
+
+console = Console()
+
+
+def display_progress(iteration, total):
+    bar_max_width = 45  # chars
+    bar_current_width = bar_max_width * iteration // total
+    bar = "█" * bar_current_width + "-" * (bar_max_width - bar_current_width)
+    progress = "%.1f" % (iteration / total * 100)
+    console.print(f"|{bar}| {progress} %", end="\r", style="bold green")
+    if iteration == total:
+        print()
+
+
+def extract_json_data(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    return data
+
+
+def threadpool_executer(function, iterable, iterable_length):
+    number_of_workers = os.cpu_count()
+    print(f"\nRunning using {number_of_workers} workers.\n")
+    with ThreadPool(number_of_workers) as pool:
+        for loop_index, _ in enumerate(pool.imap(function, iterable), 1):
+            display_progress(loop_index, iterable_length)
